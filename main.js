a63523aac6717940fd08706f1b9973ac932a3853
snkdrel <ces.zop@gmail.com>
2022-05-23 13:09:04 -0500

    Create functions for basic operations

---
+++ main.js
@@ -0,0 +1,16 @@
+// Basic operations
+let add = function(a, b){
+    return a + b;
+}
+
+let subtract = function(a, b){
+    return a - b;
+}
+
+let multiply = function(a, b){
+    return a * b;
+}
+
+let divide = function(a, b){
+    return a / b;
+}
