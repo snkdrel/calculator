62fef83537fab5529f16b911e5428db5df31910a
snkdrel <ces.zop@gmail.com>
2022-05-23 13:23:34 -0500

    Create operate function
    Take an operand and two number, and call the corresponding function for
    the operation.

--- main.js
+++ main.js
@@ -14,3 +14,25 @@ let multiply = function(a, b){
 let divide = function(a, b){
     return a / b;
 }
+
+// operate
+let operate = function(operator, numb1, numb2){
+    let res;
+    switch(operator){
+        case '+':
+            res = add(numb1, numb2);
+            break;
+        case '-':
+            res = subtract(numb1, numb2);
+            break;
+        case '*':
+            res = multiply(numb1, numb2);
+            break;
+        case '/':
+            res = divide(numb1, numb2);
+            break;
+        default:
+            alert('Invalid operation');
+    }
+    return res;
+}
